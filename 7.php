350ad399eb6f917bb0f37c0ad4dc38308774d6ec
bietml <45572045+bietml@users.noreply.github.com>
2018-12-10 00:01:29 +0530

    Add files via upload

---
+++ 7.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE HTML>
+<html>
+<!--
+<style>
+body{
+	background-color: black;
+}
+p{
+	color:white;
+	font-size:90px;
+}
+</style>
+-->
+<meta http-equiv="refresh" content="1"/>
+<p><?php 
+date_default_timezone_set('Asia/Kolkata');
+echo date("h:i:s A");?></p>
+
+</html>
